0c54011f93636148edebdbb1f0be86a27a4987e2
alexandrepiata <alexandrepiata@gmail.com>
2022-06-28 17:16:52 -0300

    finalização do projeto

--- clima-e-tempo/src/components/moleculas/card/index.js
+++ clima-e-tempo/src/components/moleculas/card/index.js
@@ -1,8 +1,82 @@
-import { CardWrapper, CardTitle } from "./styles";
-function Card(props) {
+import React, { useState, useEffect } from "react";
+import api from "../../../services";
+
+import {
+  CardWrapper,
+  CardTitle,
+  TemperatureContainer,
+  Temperature,
+  InfoContainer,
+  Devider,
+  Button,
+  Space,
+  Div,
+} from "./styles";
+function Card() {
+  const [data, setDate] = useState(null);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = () => {
+    const { geolocation } = navigator;
+    if (geolocation) {
+      geolocation.getCurrentPosition(getCoordinates);
+    } else {
+      alert("GeoLocation not enabled");
+    }
+  };
+
+  const getCoordinates = (pos) => {
+    const { latitude, longitude } = pos.coords;
+    console.log(pos);
+    api
+      .get(
+        `?lat=${latitude}&lon=${longitude}&appid=b2f714c6cc898e6899404c300afe6947&units=metric`
+      )
+      .then((res) => {
+        setDate(res.data);
+        console.log(res.data);
+      });
+  };
+
+  if (data == null) {
+    return <div>carregando...</div>;
+  }
   return (
     <CardWrapper>
-      <CardTitle>Tempo agora em Piatã, BA</CardTitle>
+      <CardTitle>Tempo agora em {data.name}</CardTitle>
+      <TemperatureContainer>
+        <Div>
+          <img
+            alt=""
+            src={require(`../../../assets/icons/${
+              data.weather[0].icon ? data.weather[0].icon : "unknown"
+            }.png`)}
+          />
+        </Div>
+        <Temperature>{data.main.temp | ""}º</Temperature>
+      </TemperatureContainer>
+      <Devider />
+      <InfoContainer>
+        <Div>Sensação Termica: {data.main.feels_like | ""}º </Div>
+        <Div>Temperatura Máxima: {data.main.temp_max | ""}º</Div>
+        <Div>Temperatura Mínima: {data.main.temp_min | ""}º</Div>
+        <Div>Pressão: {data.main.pressure | ""} hPa</Div>
+        <Div>Humidade: {data.main.humidity | ""}%</Div>
+        <Div>Vento: {data.wind.speed} M/s</Div>
+      </InfoContainer>
+      <Devider />
+      <Space />
+      <Button
+        onClick={() => {
+          setDate(null);
+          getLocation();
+        }}
+      >
+        Atualizar
+      </Button>
     </CardWrapper>
   );
 }
--- clima-e-tempo/src/components/moleculas/card/styles.js
+++ clima-e-tempo/src/components/moleculas/card/styles.js
@@ -16,3 +16,55 @@ export const CardTitle = styled.div`
   margin: 20px;
   font-size: 20px;
 `;
+
+export const TemperatureContainer = styled.div`
+  display: flex;
+  font-weight: bold;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+export const Temperature = styled.div`
+  color: black;
+  margin: 20px;
+  font-size: 50px;
+`;
+
+export const Devider = styled.div`
+  border-top: 1px solid #bbb;
+`;
+
+export const InfoContainer = styled.div`
+  color: #707070;
+  margin: 20px;
+  font-size: 16px;
+  text-align: justify;
+  text-justify: inter-word;
+`;
+
+export const Button = styled.button`
+  background-color: #81c9fa;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  background-color: white;
+  color: #81c9fa;
+  border: 2px solid #81c9fa;
+  &:hover {
+    background-color: #81c9fa;
+    color: white;
+  }
+`;
+export const Space = styled.div`
+  margin-top: 30px;
+`;
+export const Div = styled.div`
+  margin-top: 3px;
+`;
